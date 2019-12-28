325dd858c956343fba955167e767ae0b04f4ea50
Sebastien Gohier <>
2019-12-28 17:48:24 +0100

    Add new iOS Project

---
+++ Forecast/Forecast/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Forecast
+//
+//  Created by Sebastien Gohier on 28/12/2019.
+//  Copyright © 2019 Sebastien Gohier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Forecast/ForecastTests/ForecastTests.swift
@@ -0,0 +1,34 @@
+//
+//  ForecastTests.swift
+//  ForecastTests
+//
+//  Created by Sebastien Gohier on 28/12/2019.
+//  Copyright © 2019 Sebastien Gohier. All rights reserved.
+//
+
+import XCTest
+@testable import Forecast
+
+class ForecastTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
